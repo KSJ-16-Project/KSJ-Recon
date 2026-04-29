0be30aadbdb03cc315bf08104c097a5544a114f6
ksj16 <tlsgyals7632@naver.com>
2026-04-29 11:05:40 +0900

    make little function of entry_core and middle_core

--- core/entry_core.py
+++ core/entry_core.py
@@ -1,2 +1,17 @@
-print("Hello")
-print("Hello core")
+import sys
+
+
+order = sys.stdin.readline().split()
+
+
+
+URL=order[3]
+
+
+# 명령어 입력 잘못하면 예외처리
+# 명령어 입력 잘 되면
+
+# Nmap 모듈 호출 ( 연동 필요 )
+
+
+# Crawler 호출 후 Fuzzer 모듈 호출
---
+++ core/middle_core.py
@@ -0,0 +1,12 @@
+def print_nmap_data(nmap_data):
+    print(nmap_data)
+
+
+def get_nmap_data(nmap_data):
+    print_nmap_data(nmap_data)
+
+
+
+nmap_data=["hello","shinhyo"]
+
+get_nmap_data(nmap_data)
